9e534fc20a41c365c8f8e61000a8dcf196307389
Jasonzjzhou <zhouzjiang@gmail.com>
2021-10-08 13:38:39 -0400

    localStorage to store lastname, Smith

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ $(document).ready(function(){
         $('#content h2').append('<h3> The longitude is: '+position.coords.longitude+'. </h3>');
         //$('.longitude').eq(0).html(`The longitude is: ${position.coords.longitude}`);
         $('#content h2').append('<h3> With a level of accuracy of: ' + position.coords.accuracy + ' Meters.'+'</h3>');
+        localStorage.setItem("lastname", "Smith");
         // check if a key exist in local storage.
         /* if("user" in localStorage){
             alert('yes');
