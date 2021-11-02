f69150b3c7fc0476441f1e206caa953f2d08e4b8
Jasonzjzhou <zz@myscc.ca>
2021-11-02 11:24:58 -0400

    define a construction in class contentCard

--- script.js
+++ script.js
@@ -6,10 +6,14 @@
 $(document).ready(function() {
 
     class ContentCard {
-        id;
-        title;
-        description;
-        categoryType;
+        constructor(id, title, description, categoryType) {
+            this.id = id;
+            this.title = title;
+            this.description = description;
+            this.categoryType = categoryType;
+        }
+
+
     }
 
 }
