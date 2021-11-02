3efcad7f8ba6da4e68cd37c9e976914ba77ad103
Jasonzjzhou <zz@myscc.ca>
2021-11-02 15:31:05 -0400

    modify array of object defintion

--- script.js
+++ script.js
@@ -28,18 +28,22 @@ $(document).ready(function() {
             }
 
             function toString() {
-                console.log(`<div id="content-"+this.id>`);
-                console.log(`<h4>${this.title}</h4>`);
-                console.log(`<p>${this.description}</p>`);
-                console.log(`<div>${this.categoryType}</div>`);
-                console.log("</div>");
+                let ostring = `<div id="content-"+this.id>` + `<h4>${this.title}</h4>` + `<p>${this.description}</p>` + `<div>${this.categoryType}</div>` + `</div>`;
+                console.log(ostring);
+                return ostring;
             }
         }
     }
-    let books = new ContentCard([{ id: 0, title: "Douglas MacArthur American Warrior", description: "History of American General of Army", categoryType: "History" }, { id: 1, title: "The Hobbit", description: "Tolkien's fictional universe", categoryType: "Fiction" }, { id: 2, title: "Paradise lost", description: "Satan's fall, Jesus's salvation", categoryType: "Poem" }, { id: 3, title: "HTML5 and CSS3", description: "Tutorial book", categoryType: "Textbook" }, { id: 4, title: "Sichuan Cuisine", description: "Food making", categoryType: "reference book" }]);
-    alert("This is the book！")
+    books = new Array(5);
+    books[0] = new ContentCard({ id: 0, title: "Douglas MacArthur American Warrior", description: "History of American General of Army", categoryType: "History" }),
+        books[1] = new ContentCard({ id: 1, title: "The Hobbit", description: "Tolkien's fictional universe", categoryType: "Fiction" }),
+        books[2] = new ContentCard({ id: 2, title: "Paradise lost", description: "Satan's fall, Jesus's salvation", categoryType: "Poem" }),
+        books[3] = new ContentCard({ id: 3, title: "HTML5 and CSS3", description: "Tutorial book", categoryType: "Textbook" }),
+        books[4] = new ContentCard({ id: 4, title: "Sichuan Cuisine", description: "Food making", categoryType: "reference book" });
+    console.log(books[0]);
     for (let x of books) {
-        $("#content-list").html(x);
+        updateContent(x.title, x.description, x.categoryType);
+        console.log(x.title, x.description, x.categoryType)
+            // $("#content-list").html(toString());
     }
-
 });
