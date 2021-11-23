c0863a35651743a6fdce7917020a5d0b61018b74
Jasonzjzhou <zz@myscc.ca>
2021-11-22 23:32:16 -0500

    update script file and json file

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 $(function() {
-    $.getJSON("movies.json", function(data) {
-        let results = [];
-        movie.forEach(function(data) {
-            results.push(data);
-        });
-        let $tableBody = $('<tbody></tbody>');
-        for (let i = 0; i < results.length; i++) {
-            let $row = $('<tr></tr>');
-            $row.append($('<td></td>').text(data.title));
-            $row.append($('<td></td>').text(data.releasingDate));
-            $row.append($('<td></td>').text(data.director));
-            $row.append($('<td></td>').text(data.rating));
-        }
-        $('thead').after($tableBody);
-    }).fail(function() {
-        console.log("An error has occurred.");
+
+    let results = [];
+    $.get("movies.json", function(data) {
+        // let $tableBody = $('<tbody></tbody>');
+        console.log(data.movies.length);
+        // for (let i = 0; i < data.movies.length; i++) {
+        //     let $row = $('<tr></tr>');
+        //     $row.append($('<td></td>').text(data.title));
+        //     $row.append($('<td></td>').text(data.releasingDate));
+        //     $row.append($('<td></td>').text(data.director));
+        //     $row.append($('<td></td>').text(data.rating));
+        //     $('thead').after($tableBody);
+        // }
     });
 });
