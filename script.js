fb2f0812805ead4268a95d017a2b287546cca229
Jasonzjzhou <zz@myscc.ca>
2021-11-22 21:40:49 -0500

    modify index.html

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 $(function() {
     $.getJSON("Movies.json", function(data) {
+        let results = [];
+        Movies.forEach(function(movie))
         console.log(data.title);
         console.log(data.releasingDate);
         console.log(data.director);
