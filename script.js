9d8b11ee798757a391a848da4415097c9b564f19
Jasonzjzhou <zz@myscc.ca>
2021-11-22 21:25:54 -0500

    update the html file

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+$(function() {
+    $.getJSON("Movies.json", function(data) {
+        console.log(data.title);
+        console.log(data.releasingDate);
+        console.log(data.director);
+        console.log(data.rating);
+    }).fail(function() {
+        console.log("An error has occurred.");
+    });
+});
